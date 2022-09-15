b6f6a357c90c521eea00fecbe5a2ab262e09fa2a
KimiMasa1021 <k.massan46@gmail.com>
2022-09-15 23:34:19 +0900

    aa

---
+++ android/app/src/main/kotlin/com/example/account_book_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.account_book_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
