9da03bb7af590a678fd35e922c7585de3a80d0a5
KiminaritaMasahiro <m.kiminarita.sys21@morijyobi.ac.jp>
2022-12-13 21:45:15 +0900

    aaa

--- lib/view/auth/sign_up.dart
+++ lib/view/auth/sign_up.dart
@@ -1,11 +1,7 @@
 import 'package:account_book_app/component/auth/normal_button.dart';
 import 'package:account_book_app/provider/general_provider.dart';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import '../../constant/hex_color.dart';
 import '../theme/app_theme.dart';
 
 class SignUp extends HookConsumerWidget {
