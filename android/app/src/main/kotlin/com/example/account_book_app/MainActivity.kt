268a56c741e4fde4c28904e61f50bb3ded4f6e68
KimiMasa1021 <k.massan46@gmail.com>
2023-02-24 23:58:25 +0900

    スプラッシュスクリーンの修正

--- android/app/src/main/kotlin/com/example/account_book_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/account_book_app/MainActivity.kt
@@ -3,5 +3,4 @@ package com.kmass.account_book_app
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.SplashScreen
 class MainActivity: FlutterActivity() {    
-    override fun provideSplashScreen(): SplashScreen? = CustomSplashScreen()
 }
